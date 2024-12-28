4a8b0b1d6c42c21f9d1e13af613f60ca890fa4df
Vitalii <vitalii.gaponov@wirenboard.com>
2024-12-28 14:49:37 +0300

    feat: start file for base object topic manager

---
+++ src/topicmgr-main.mod.js
@@ -0,0 +1,7 @@
+/**
+ * @file Модуль с описанием базового объекта менеджера топиков.
+ *       Данный объект нужен как фундамент для наслоения расширений
+ * 
+ * @author Vitalii Gaponov <vitalii.gaponov@wirenboard.com>
+ * @link Комментарии в формате JSDoc <https://jsdoc.app/>
+ */
