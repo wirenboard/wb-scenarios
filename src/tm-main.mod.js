b11ee9b5c334d5704c8d6539f2c71a5cfc084bef
Vitalii <vitalii.gaponov@wirenboard.com>
2025-01-25 19:10:17 +0300

    refactor: fix typos and format with eslint

--- src/tm-main.mod.js
+++ src/tm-main.mod.js
@@ -2,7 +2,7 @@
  * @file topicmgr-main.js
  * @description Модуль с описанием базового объекта менеджера топиков.
  *     Данный объект нужен как фундамент для наслоения расширений.
- * 
+ *
  * @author Vitalii Gaponov <vitalii.gaponov@wirenboard.com>
  * @link Комментарии в формате JSDoc <https://jsdoc.app/>
  */
@@ -26,12 +26,12 @@ function TopicManager() {
 
 /**
  * Вставка процессора в цепочку с учетом приоритета
- * 
+ *
  * @param {Array} processorsChain  Цепочка процессоров
  *     (массив объектов { fn, priority })
  * @param {Object} processorEntry Объект процессора { fn, priority }
  */
-function insertProcessorIntoChain(processorsChain , processorEntry) {
+function insertProcessorIntoChain(processorsChain, processorEntry) {
   var insertIndex = -1;
 
   // Ищем подходящее место для вставки
@@ -52,10 +52,9 @@ function insertProcessorIntoChain(processorsChain , processorEntry) {
   }
 }
 
-
 /**
  * Добавление процессора в цепочку
- * 
+ *
  * @param {Function} processor Функция процессора, добавляемая в цепочку
  * @param {number} [priority=0] (Опционально) Приоритет процессора
  *     (чем выше, тем раньше он будет вызван)
@@ -70,7 +69,7 @@ TopicManager.prototype.addProcessor = function (processor, priority) {
   // Создаем объект для процессора с приоритетом по умолчанию если не указан
   var processorEntry = {
     fn: processor,
-    priority: priority || 0
+    priority: priority || 0,
   };
   insertProcessorIntoChain(this.pluginsProcessorsChain, processorEntry);
 
@@ -80,7 +79,7 @@ TopicManager.prototype.addProcessor = function (processor, priority) {
 
 /**
  * Удаление обработчика из цепочки
- * 
+ *
  * @param {Function} processor Функция процессора которую нужно удалить
  */
 TopicManager.prototype.removeProcessor = function (processor) {
@@ -102,7 +101,7 @@ TopicManager.prototype.removeProcessor = function (processor) {
 
 /**
  * Обработка данных всеми плагинами
- * 
+ *
  * @param {string} topic - Имя топика
  * @param {*} newValue - Новое значение топика
  */
@@ -141,8 +140,7 @@ TopicManager.prototype.initRulesForAllTopics = function (ruleName) {
   }
 
   // Создаем правило
-  this.ruleId = defineRule(
-    ruleName, {
+  this.ruleId = defineRule(ruleName, {
     whenChanged: topics,
     then: function (newValue, devName, cellName) {
       var topic = devName + '/' + cellName;
@@ -155,15 +153,17 @@ TopicManager.prototype.initRulesForAllTopics = function (ruleName) {
     return false;
   }
 
-  log.debug('Rule "' + ruleName + '" successfully created with ID:', this.ruleId);
+  log.debug(
+    'Rule "' + ruleName + '" successfully created with ID:',
+    this.ruleId
+  );
   return true;
 };
-
 
 /**
  * Метод для подключения плагинов к объекту TopicManager
  * Ожидает объект с методом install
- * 
+ *
  * @param {Object} plugin - Объект плагина с методом install
  * @param {Object} [options] - Опциональные параметры для плагина
  * @returns {boolean} Успешность установки плагина
@@ -189,15 +189,19 @@ TopicManager.prototype.installPlugin = function (plugin, options) {
   }
 
   // Проверка зависимостей
-  var isPluginHaveDep = (Array.isArray(plugin.dependencies) && plugin.dependencies.length > 0);
+  var isPluginHaveDep =
+    Array.isArray(plugin.dependencies) && plugin.dependencies.length > 0;
   if (isPluginHaveDep === true) {
     for (var i = 0; i < plugin.dependencies.length; i++) {
       var depName = plugin.dependencies[i];
-      var isDepInstalled = (this.installedPlugins.indexOf(depName) !== -1);
+      var isDepInstalled = this.installedPlugins.indexOf(depName) !== -1;
       if (isDepInstalled === false) {
         log.error(
-          'Plugin "' + plugin.name + '" depends on "' + depName +
-          '", but it is not installed yet.'
+          'Plugin "' +
+            plugin.name +
+            '" depends on "' +
+            depName +
+            '", but it is not installed yet.'
         );
         return false;
       }
