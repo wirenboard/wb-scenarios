5f14493f3dffd381628aed6f77f716287c00fe12
Vitalii <vitalii.gaponov@wirenboard.com>
2025-02-01 01:52:21 +0300

    refactor: event resolvers plugin prettier auto format

--- src/tm-event-resolvers.mod.js
+++ src/tm-event-resolvers.mod.js
@@ -1,7 +1,7 @@
 /**
  * @file tm-event-resolvers.mod.js
  * @description Модуль таблицы регистрируемых событий над контролами
- * 
+ *
  * @author Vitalii Gaponov <vitalii.gaponov@wirenboard.com>
  * @link Комментарии в формате JSDoc <https://jsdoc.app/>
  */
@@ -19,7 +19,7 @@
  * @returns {boolean} Возвращает true, если контрол включен
  */
 function whenEnabled(topic, cfg, ctx) {
-  var isEventTriggered = (topic.val.new === true);
+  var isEventTriggered = topic.val.new === true;
   return isEventTriggered;
 }
 
@@ -29,7 +29,7 @@ function whenEnabled(topic, cfg, ctx) {
  * @returns {boolean} Возвращает true, если контрол выключен
  */
 function whenDisabled(topic, cfg, ctx) {
-  var isEventTriggered = (topic.val.new === false);
+  var isEventTriggered = topic.val.new === false;
   return isEventTriggered;
 }
 
@@ -50,15 +50,15 @@ function whenChange(topic, cfg, ctx) {
  *     стало больше границы указанной пользователем в cfg.actionValue
  */
 function whenCrossUpper(topic, cfg, ctx) {
-  var isPrevTypeNumber = (typeof topic.val.prev === 'number');
+  var isPrevTypeNumber = typeof topic.val.prev === 'number';
   if (isPrevTypeNumber !== true) {
     // Если prev отсутствует (null) или имеет некорректный тип,
     // событие не может быть обработано
     return false;
   }
 
-  var isEventTriggered = (topic.val.new > cfg.actionValue) &&
-                         (topic.val.prev <= cfg.actionValue);
+  var isEventTriggered =
+    topic.val.new > cfg.actionValue && topic.val.prev <= cfg.actionValue;
 
   return isEventTriggered;
 }
@@ -70,64 +70,64 @@ function whenCrossUpper(topic, cfg, ctx) {
  *     стало меньше границы указанной пользователем в cfg.actionValue
  */
 function whenCrossLower(topic, cfg, ctx) {
-  var isPrevTypeNumber = (typeof topic.val.prev === 'number');
+  var isPrevTypeNumber = typeof topic.val.prev === 'number';
   if (isPrevTypeNumber !== true) {
     // Если prev отсутствует (null) или имеет некорректный тип,
     // событие не может быть обработано
     return false;
   }
 
-  var isEventTriggered = (topic.val.new < cfg.actionValue) &&
-                         (topic.val.prev >= cfg.actionValue);
+  var isEventTriggered =
+    topic.val.new < cfg.actionValue && topic.val.prev >= cfg.actionValue;
 
   return isEventTriggered;
 }
 
 /**
-* Таблица описания разных типов событий
-* Содержит имя события и соответствующие ему:
-* - reqCtrlTypes: Разрешенные типы контроллов с которыми может работать событие
-* - launchResolver: Функция резолвера события - решает произошло ли собтие
-* - resetResolverName: Строка имени обратного события
-* - resetResolver: Функция обратного резолвера
-*/
+ * Таблица описания разных типов событий
+ * Содержит имя события и соответствующие ему:
+ * - reqCtrlTypes: Разрешенные типы контроллов с которыми может работать событие
+ * - launchResolver: Функция резолвера события - решает произошло ли собтие
+ * - resetResolverName: Строка имени обратного события
+ * - resetResolver: Функция обратного резолвера
+ */
 var registryEventResolvers = {
-  'whenChange': {
+  whenChange: {
     reqCtrlTypes: ['switch', 'value'],
     launchResolver: whenChange,
     resetResolverName: null, // Не существует противоположного события
-    resetResolver: null // Не существует противоположного события
+    resetResolver: null, // Не существует противоположного события
   },
-  'whenDisabled': {
+  whenDisabled: {
     reqCtrlTypes: ['switch'],
     launchResolver: whenDisabled,
     resetResolverName: 'whenEnabled',
-    resetResolver: null // Вычисляется ниже динамически
+    resetResolver: null, // Вычисляется ниже динамически
   },
-  'whenEnabled': {
+  whenEnabled: {
     reqCtrlTypes: ['switch'],
     launchResolver: whenEnabled,
     resetResolverName: 'whenDisabled',
-    resetResolver: null // Вычисляется ниже динамически
+    resetResolver: null, // Вычисляется ниже динамически
   },
-  'whenCrossUpper': {
+  whenCrossUpper: {
     reqCtrlTypes: ['value'],
     launchResolver: whenCrossUpper,
     resetResolverName: 'whenCrossLower',
-    resetResolver: null // Вычисляется ниже динамически
+    resetResolver: null, // Вычисляется ниже динамически
   },
-  'whenCrossLower': {
+  whenCrossLower: {
     reqCtrlTypes: ['value'],
     launchResolver: whenCrossLower,
     resetResolverName: 'whenCrossUpper',
-    resetResolver: null // Вычисляется ниже динамически
-  }
+    resetResolver: null, // Вычисляется ниже динамически
+  },
   // @todo: add whileAbove, whileBelow
 };
 
 /**
  * Автоматическое заполнение resetResolver
- * 
+ *
  * После базового заполнения структуры резолверов нужно вычислить
  * resetResolver для всех типов действий  на основе resetResolverName
  * и заполнить это поле
@@ -136,19 +136,20 @@ Object.keys(registryEventResolvers).forEach(function (key) {
   var resolver = registryEventResolvers[key];
   if (!resolver.resetResolverName) {
     log.warning(
-      'resetResolverName для действия "' +
-      key + '" не установлен'
+      'resetResolverName для действия "' + key + '" не установлен'
     );
     return;
   }
   if (!registryEventResolvers[resolver.resetResolverName]) {
     log.warning(
       'Ошибка: resetResolverName для действия "' +
-      key + '" указан, но отсутствует в реестре действий'
+        key +
+        '" указан, но отсутствует в реестре действий'
     );
     return;
   }
-  resolver.resetResolver = registryEventResolvers[resolver.resetResolverName].launchResolver;
+  resolver.resetResolver =
+    registryEventResolvers[resolver.resetResolverName].launchResolver;
 });
 
 exports.registryEventResolvers = registryEventResolvers;
