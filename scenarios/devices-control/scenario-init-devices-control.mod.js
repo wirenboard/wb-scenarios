ff245e9f2db448ff421fe2255b1799937f066faa
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-07-15 21:16:58 +0300

    refactor: add change to use class

---
+++ scenarios/devices-control/scenario-init-devices-control.mod.js
@@ -0,0 +1,122 @@
+/**
+ * @file init-devices-control.js - ES5 script for wb-rules v2.28
+ * @description Script for init scenarios of the SCENARIO_TYPE_STR type
+ *     This script:
+ *     - Loads all scenario configurations of the specific type from a file
+ *     - Initializes them according to the settings specified in each scenario
+ *
+ * @author Vitalii Gaponov <vitalii.gaponov@wirenboard.com>
+ */
+
+var scHelpers = require('scenarios-general-helpers.mod');
+var CustomTypeSc = require('devices-control.mod').InputOutputLinkScenario;
+var Logger = require('logger.mod').Logger;
+
+/**
+ * Scenario initialization configuration parameters
+ * @typedef {Object} ScenarioConfig
+ */
+var CFG = {
+  reqVerGeneralCfg: 1, // Required version of common config structure
+  reqVerScenario: 1, // Required version of this scenario type config
+  configPath: '/etc/wb-scenarios.conf',
+  scenarioTypeStr: 'devicesControl'
+};
+
+var log = new Logger('WBSC-' + CFG.scenarioTypeStr + '-init');
+
+/**
+ * Initializes a scenario using the specified settings
+ * @param {object} scenarioCfg - The scenario object containing the settings
+ * @returns {void}
+ */
+function initializeScenario(scenarioCfg) {
+  log.debug('Processing scenario config: "{}"', JSON.stringify(scenarioCfg));
+
+  var scenario = new CustomTypeSc();
+  var cfg = {
+    idPrefix: scenarioCfg.id_prefix,
+    inControls: scenarioCfg.inControls,
+    outControls: scenarioCfg.outControls
+  };
+
+  try {
+    var isInitSuccess = scenario.init(scenarioCfg.name, cfg);
+
+    if (isInitSuccess !== true) {
+      log.error(
+        'Init operation aborted for scenario name: "{}" with idPrefix: "{}"',
+        scenarioCfg.name,
+        scenarioCfg.id_prefix
+      );
+      return;
+    }
+
+    log.debug(
+      'Initialization successful for scenario name: "{}" with idPrefix: "{}"',
+      scenarioCfg.name,
+      scenarioCfg.id_prefix
+    );
+
+    var scenarioStorage = scHelpers.getGlobalScenarioStore(
+      CFG.scenarioTypeStr
+    );
+    scenarioStorage[scenarioCfg.id_prefix] = scenario;
+    log.debug('Stored in global registry with ID: {}', scenarioCfg.id_prefix);
+  } catch (error) {
+    log.error(
+      'Exception during scenario initialization: "{}" for scenario: "{}"', 
+      error.message || error, 
+      scenarioCfg.name
+    );
+  }
+}
+
+/**
+ * Find and return all enabled scenarios of the requested type
+ * @param {Array} listScenario - Array of all scenarios from the config
+ * @param {string} searchScenarioType - Scenario type we are looking for
+ * @returns {Array} Array of active scenarios of that type
+ */
+//TODO: Need delete not actual activate function from config WEBUI
+//      Then we can use standart scHelpers.findAllScenariosWithType()
+function findAllActiveScenariosWithType(listScenario, searchScenarioType) {
+  var matchedScenarios = [];
+  for (var i = 0; i < listScenario.length; i++) {
+    var scenario = listScenario[i];
+    var isTarget = (scenario.scenarioType === searchScenarioType) &&
+      (scenario.enable === true);
+    if (isTarget) {
+      matchedScenarios.push(scenario);
+    }
+  }
+  return matchedScenarios;
+}
+
+function setup() {
+  log.debug('Start initialisation "{}" type scenarios', CFG.scenarioTypeStr);
+  var listAllScenarios = scHelpers.readAndValidateScenariosConfig(
+    CFG.configPath,
+    CFG.reqVerGeneralCfg
+  );
+  if (!listAllScenarios) return;
+
+  var matchedScenarios = findAllActiveScenariosWithType(
+    listAllScenarios,
+    CFG.scenarioTypeStr
+  );
+  if (matchedScenarios.length === 0) {
+    log.debug(
+      'No correct and active scenarios of type "{}" found',
+      CFG.scenarioTypeStr
+    );
+    return;
+  }
+
+  log.debug('Number of matched scenarios: {}', matchedScenarios.length);
+  for (var i = 0; i < matchedScenarios.length; i++) {
+    initializeScenario(matchedScenarios[i]);
+  }
+}
+
+exports.setup = setup;
