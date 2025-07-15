638e00930cd7abc51d31758b7f6de3f139a81850
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-07-15 21:26:09 +0300

    fix: add new module name

--- scenarios/scenario-init-main.js
+++ scenarios/scenario-init-main.js
@@ -1,4 +1,4 @@
-var DevicesControlSetup = require("init-devices-control.mod").setup;
+var DevicesControlSetup = require("scenario-init-devices-control.mod").setup;
 var LightControlSetup = require("scenario-init-light-control.mod").setup;
 var ThermostatSetup = require("scenario-init-thermostat.mod").setup;
 var Logger = require('logger.mod').Logger;
