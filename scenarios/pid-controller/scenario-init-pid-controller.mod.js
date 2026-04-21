9e7fa03eb2c5933344ffbdc006d2446215d115ff
Валерий <trofimof09@mail.ru>
2026-04-21 16:36:03 +0300

    feat: add pid controller scenario INT-982 (#81)
    * feat: add pid analog scenario INT-982
    
    * update name and add precision
    
    * fix
    
    * update and lint
    
    * first review iteration
    
    * revert schema
    
    * revert schema2
    
    * fix typo

---
+++ scenarios/pid-controller/scenario-init-pid-controller.mod.js
@@ -0,0 +1,107 @@
+/**
+ * @file scenario-init-pid-controller.mod.js - ES5 module for wb-rules v2.34
+ * @description Script for init scenarios of the SCENARIO_TYPE_STR type
+ *     This script:
+ *     - Loads all scenario configurations of the specific type from a file
+ *     - Initializes them according to the settings specified in each scenario
+ *
+ * @author Valerii Trofimov <valeriy.trofimov@wirenboard.com>
+ */
+
+var scHelpers = require('scenarios-general-helpers.mod');
+var CustomTypeSc = require('pid-controller.mod').PidControllerScenario;
+var Logger = require('logger.mod').Logger;
+
+/**
+ * Scenario initialization configuration parameters
+ * @typedef {Object} ScenarioConfig
+ */
+var CFG = {
+  reqVerGeneralCfg: 1, // Required version of common config structure
+  reqVerScenario: 1, // Required version of this scenario type config
+  configPath: '/etc/wb-scenarios.conf', // TODO(Valerii): Need refactor into a constant
+  scenarioTypeStr: 'pidController',
+};
+
+var log = new Logger('WBSC-' + CFG.scenarioTypeStr + '-init');
+
+/**
+ * Initializes a single PID controller scenario
+ * @param {Object} scenarioCfg - Scenario config object from wb-scenarios.conf
+ * @returns {void}
+ */
+function initializeScenario(scenarioCfg) {
+  log.debug('Processing scenario config: "{}"', JSON.stringify(scenarioCfg));
+
+  var scenario = new CustomTypeSc();
+  var cfg = {
+    idPrefix: scenarioCfg.idPrefix,
+    sensor: scenarioCfg.sensor,
+    setpoint: scenarioCfg.setpoint,
+    setpointLimits: scenarioCfg.setpointLimits,
+    pidCoefficients: scenarioCfg.pidCoefficients,
+    calculationPeriodSec: scenarioCfg.calculationPeriodSec,
+    deadBand: scenarioCfg.deadBand,
+    actuators: scenarioCfg.actuators,
+  };
+
+  try {
+    // Returns true if VD created successfully; full initialization continue asynchronously
+    var isBasicVdCreated = scenario.init(scenarioCfg.name, cfg);
+    if (isBasicVdCreated !== true) {
+      log.error(
+        'Virtual device creation failed for scenario name: "{}" with idPrefix: "{}"',
+        scenarioCfg.name,
+        scenario.idPrefix
+      );
+      return;
+    }
+
+    log.debug(
+      'VD created successfully, init continue asynchronously for scenario name: "{}" with idPrefix: "{}"',
+      scenarioCfg.name,
+      scenario.idPrefix
+    );
+
+    var scenarioStorage = scHelpers.getGlobalScenarioStore(
+      CFG.scenarioTypeStr
+    );
+    scenarioStorage[scenario.idPrefix] = scenario;
+    log.debug('Stored in global registry with ID: ' + scenario.idPrefix);
+  } catch (error) {
+    log.error(
+      'Exception during scenario initialization: "{}" for scenario: "{}"',
+      error.message || error,
+      scenarioCfg.name
+    );
+  }
+}
+
+function setup() {
+  log.debug('Start initialisation "{}" type scenarios', CFG.scenarioTypeStr);
+  var listAllScenarios = scHelpers.readAndValidateScenariosConfig(
+    CFG.configPath,
+    CFG.reqVerGeneralCfg
+  );
+  if (!listAllScenarios) return;
+
+  var matchedScenarios = scHelpers.findAllScenariosWithType(
+    listAllScenarios,
+    CFG.scenarioTypeStr,
+    CFG.reqVerScenario
+  );
+  if (matchedScenarios.length === 0) {
+    log.debug(
+      'No correct and active scenarios of type "{}" found',
+      CFG.scenarioTypeStr
+    );
+    return;
+  }
+
+  log.debug('Number of matched scenarios: {}', matchedScenarios.length);
+  for (var i = 0; i < matchedScenarios.length; i++) {
+    initializeScenario(matchedScenarios[i]);
+  }
+}
+
+exports.setup = setup;
